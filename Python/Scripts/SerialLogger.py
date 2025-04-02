46832f3ea4f3d0137ae04c224134d1d79712a93c
Sidhu <150897300+zydusX3@users.noreply.github.com>
2025-04-02 12:25:55 +0530

    Update SerialLogger.py

--- Python/Scripts/SerialLogger.py
+++ Python/Scripts/SerialLogger.py
@@ -1,26 +1,27 @@
-import serial
-import time
-from datetime import datetime
+############################# Simple implementation ############################################
+# import serial
+# import time
+# from datetime import datetime
 
-port = "COM22"
-baud = 115200
-date_str = datetime.now().strftime("%d%m%Y")
-filename = f"arduino_log_{date_str}.txt"
+# port = "COM22"
+# baud = 115200
+# date_str = datetime.now().strftime("%d%m%Y")
+# filename = f"arduino_log_{date_str}.txt"
 
-with serial.Serial(port, baud, timeout=1) as ser, open(filename, "a") as log:
-    print(f"Logging from {port} at {baud} baud...")
-    print(f"Saving to file: {filename}")
-    try:
-        while True:
-            line = ser.readline().decode(errors='ignore').strip()
-            if line:
-                print(line)
-                log.write(line+"\n")
-    except KeyboardInterrupt:
-        print("Logging stopped by User")
+# with serial.Serial(port, baud, timeout=1) as ser, open(filename, "a") as log:
+#     print(f"Logging from {port} at {baud} baud...")
+#     print(f"Saving to file: {filename}")
+#     try:
+#         while True:
+#             line = ser.readline().decode(errors='ignore').strip()
+#             if line:
+#                 print(line)
+#                 log.write(line+"\n")
+#     except KeyboardInterrupt:
+#         print("Logging stopped by User")
 
 
-#888888888888888888888888888888888888888888888888888888888888888888888888888888#
+############################# Auto Baudrate setting and Port detection #########################
 import serial
 import serial.tools.list_ports
 import time
